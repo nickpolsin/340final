84933386bb086892f9364d1b4d4856a03b4433fc
JacksonMaxfield <jackson.brown.825@gmail.com>
2016-06-03 01:50:05 -0700

    commented out issues

--- static/js/news.js
+++ static/js/news.js
@@ -34,8 +34,11 @@ $(document).ready(function() {
     var index = quotes_data.length - 1;
 
     for (i = index; i > index - 5; i--) {
-      $("#quote-" + (i - (index - 1))).html(quotes_data[i].quote);
-      $("#quote-" + (i - (index - 1)) + "-tag").html(quotes_data.first_name + " " quotes_data.last_name + ", " quotes_data.policy);
+      var tempQtId = "#quote-" + (i - (index - 1));
+      var tempQttId = "#quote-" + (i - (index - 1)) + "-tag";
+
+      //$().html(quotes_data[i].quote);
+      //$().html(quotes_data.first_name + " " quotes_data.last_name + ", " quotes_data.policy);
     }
   };
 });
