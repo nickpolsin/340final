8805e5b485eb22b3b30a50bc33862b43fbe88946
JacksonMaxfield <jackson.brown.825@gmail.com>
2016-06-03 12:11:22 -0700

    finalizing bar.js

--- static/js/bar.js
+++ static/js/bar.js
@@ -1,14 +1,6 @@
 var count_data;
 
 $(document).ready(function() {
-  var dc_data = {
-    clinton_dc_bar: 1769,
-    sanders_dc_bar: 1499,
-    trump_dc_bar: 1239,
-    cruz_dc_bar: 559,
-    kasich_dc_bar: 161
-  };
-
   $.ajax({
     type: 'get',
     url: '/Qcounts',
@@ -16,21 +8,18 @@ $(document).ready(function() {
     success: loadCounts
   });
 
-  var i = 0;
-
-  for (var prop in dc_data) {
+  for (i = 0; i < count_data.length; i++) {
+    var barId = "#" + count_data[i].last_name.toLowerCase() + "_dc_bar";
+    var count = parseInt(count_data[i].count);
     if (i < 2) {
-      $("#" + prop).css("width", "" + (dc_data[prop] / 11.915));
+      $(barId).css("width", "" + (count / 11.915));
     } else {
-      $("#" + prop).css("width", "" + (dc_data[prop] / 6.185));
+      $(barId).css("width", "" + (count / 6.185));
     }
-
-    i++;
   };
 });
 
 function loadCounts(data) {
-  console.log("returned data: ", data);
   count_data = data.candidates;
   console.log("applying data to own variable", count_data);
 };
