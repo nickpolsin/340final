07e0bd89b3b3a08c5a63e4ea8af26b86156858d7
JacksonMaxfield <jackson.brown.825@gmail.com>
2016-06-02 17:13:53 -0700

    initial commit

---
+++ cmd/campaign/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+var (
+	db *sql.DB
+)
+
+// Candidate stores coordinates as numeric strings
+type Candidate struct {
+	Name      string `json:"name"`
+	Party     string `json:"party"`
+	Birthday  string `json:"birthdate"`
+	Residence string
+	Education string
+}
+
+// keeping it simple we'll hard code the query
+func getCandidates() []Candidate {
+	// Store the results
+
+	var candidates []Candidate
+	// For simplicity the search area is hardcoded rather than coming from the user
+
+	rows, err := db.Query("SELECT birthday, residence, education FROM candidate")
+	if err != nil {
+		return nil
+	}
+
+	for rows.Next() {
+		// for each row, we create an empty Location object
+		var candidate Candidate
+
+		// go can scan the columns returned from the select directly into the properties from our object
+		// we need &loc.xxx so that scan can update the properties in memory (&loc.Name means address of the Name property for this instance of loc)
+		err = rows.Scan(&candidate.Birthday, &candidate.Residence, &candidate.Education)
+		if err != nil {
+			return nil
+		}
+		// append each intermediate loc to our array
+		candidates = append(candidates, candidate)
+	}
+	rows.Close()
+
+	return candidates
+}
+
+// This handler will take care of GET and POST requests
+func candidatesHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	default: // We're only supporting GET in this incarnation
+		candidates := getCandidates()
+
+		// This struct holds all of the context that my index template needs to render
+		context := struct {
+			Candidates []Candidate `json:"candidates"`
+		}{
+			candidates,
+		}
+
+		jsonified, err := json.Marshal(context)
+		if err != nil {
+			panic(err)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jsonified)
+	}
+
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+	var errd error
+	db, errd = sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if errd != nil {
+		log.Fatalf("Error opening database: %q", errd)
+	}
+
+	server := http.Server{
+		Addr: ":" + port,
+	}
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/bios", candidatesHandler)
+	server.ListenAndServe()
+}
