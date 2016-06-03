c7bb2d8849be7cf6da8849282421a1b231d27918
JacksonMaxfield <jackson.brown.825@gmail.com>
2016-06-03 01:44:56 -0700

    working on quotes first five

--- static/js/news.js
+++ static/js/news.js
@@ -27,4 +27,15 @@ $(document).ready(function() {
 
   console.log("all quotes recieved: ", quotes_data);
   console.log("all articles recieved: ", articles_data);
+
+  pushQuotes();
 });
+
+function pushQuotes() {
+  var index = quotes_data.length - 1;
+
+  for (i = index; i > index - 5; i--) {
+    $("#quote-" + (i - (index - 1))).html(quotes_data[i].quote);
+    $("#quote-" + (i - (index - 1)) + "-tag").html(quotes_data.first_name + " " quotes_data.last_name + ", " quotes_data.policy);
+  }
+};
