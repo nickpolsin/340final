24b5a7ade9cfdbff9cf8891ae7e45fb77fcc7918
JacksonMaxfield <jackson.brown.825@gmail.com>
2016-06-03 01:52:04 -0700

    logging test

--- static/js/news.js
+++ static/js/news.js
@@ -37,6 +37,9 @@ $(document).ready(function() {
       var tempQtId = "#quote-" + (i - (index - 1));
       var tempQttId = "#quote-" + (i - (index - 1)) + "-tag";
 
+      console.log("QuoteID text: ", tempQtId);
+      console.log("QuoteID tag: ", tempQttId);
+
       //$().html(quotes_data[i].quote);
       //$().html(quotes_data.first_name + " " quotes_data.last_name + ", " quotes_data.policy);
     }
