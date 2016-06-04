150e5f61a3afd4ba8e30dadd932df531a2b4ccbf
JacksonMaxfield <jackson.brown.825@gmail.com>
2016-06-03 17:36:03 -0700

    forgot to add

--- static/js/news.js
+++ static/js/news.js
@@ -54,7 +54,7 @@ function shoveArticles() {
       tempImgSource = "resources/img/nyt.png";
     }
 
-    var tempTag = "<b>"articles_data[i].title + "</b><br>by: " + articles_data[i].author_fn.toLowerCase() + " " + articles_data[i].author_ln.toLowerCase() + ", published: " + articles_data[i].pub_date.substr(6);
+    var tempTag = "<b>" + articles_data[i].title + "</b><br>by: " + articles_data[i].author_fn.toLowerCase() + " " + articles_data[i].author_ln.toLowerCase() + ", published: " + articles_data[i].pub_date.substr(6);
 
     $(tempAtleImgId).attr("src", tempImgSource);
     $(tempAtleTagId).html(tempTag);
