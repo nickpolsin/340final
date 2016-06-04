5cca42642408977cc25c38806e4337b0e08c27e0
JacksonMaxfield <jackson.brown.825@gmail.com>
2016-06-03 22:04:15 -0700

    reverting back to pre gin

--- cmd/campaign/insertArticle.go
+++ cmd/campaign/insertArticle.go
@@ -5,16 +5,15 @@ import (
 	// this is Go's built-in sql library
 	"database/sql"
 	"log"
-	"net/http"
 	"os"
 
 	// this allows us to run our web server
-	"github.com/gin-gonic/gin"
+
 	// this lets us connect to Postgres DB's
 	_ "github.com/lib/pq"
 )
 
-func main() {
+func insertArticle() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("$PORT must be set")
@@ -27,23 +26,4 @@ func main() {
 	if errd != nil {
 		log.Fatalf("Error opening database: %q", errd)
 	}
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("html/*")
-	router.Static("/static", "static")
-
-	router.GET("/new-article.html", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "new-article.html", nil)
-	})
-
-	router.POST("/Qnew-article", func(c *gin.Context) {
-		publisher := c.PostForm("publisher")
-		authorfirstname := c.PostForm("authorfirstname")
-		authorlastname := c.PostForm("authorlastname")
-		datepublished := c.PostForm("datepublished")
-		link := c.PostForm("link")
-		title := c.PostForm("title")
-		db.Query("INSERT INTO article (publisher, authorfirstname, authorlastname, datepublished, link, title) VALUES ($1, $2, $3, $4, $5, $6);", publisher, authorfirstname, authorlastname, datepublished, link, title)
-		c.Data(http.StatusOK, "text/html", []byte("New Account Created"))
-	})
 }
